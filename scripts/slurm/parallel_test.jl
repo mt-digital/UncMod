14a9bbc7c13e416c4eec327c067bbf2434be1eb9
Matthew A. Turner <maturner01@gmail.com>
2021-11-17 23:04:53 -0800

    wrote run_trials script for slurm jobs

--- scripts/slurm/parallel_test.jl
+++ scripts/slurm/parallel_test.jl
@@ -5,16 +5,16 @@ num_cores = parse(Int, ENV["SLURM_CPUS_PER_TASK"])
 
 addprocs(num_cores)
 
-println("Number of cores: ", nprocs())
-println("Number of workers: ", nworkers())
+# println("Number of cores: ", nprocs())
+# println("Number of workers: ", nworkers())
 
-# Each worker gets its own id and hostname.
-for i in workers()
-    id, pid, host = fetch(@spawnat i (myid(), getpid(), gethostname()))
-    println(id, " ", pid, " ", host)
-end
+# # Each worker gets its own id and hostname.
+# for i in workers()
+#     id, pid, host = fetch(@spawnat i (myid(), getpid(), gethostname()))
+#     println(id, " ", pid, " ", host)
+# end
 
-# Clean up, remove workers.
-for i in workers()
-    rmprocs(i)
-end
+# # Clean up, remove workers.
+# for i in workers()
+#     rmprocs(i)
+# end
---
+++ scripts/slurm/run_trials.jl
@@ -0,0 +1,52 @@
+using Distributed
+
+using DrWatson
+quickactivate("..")
+
+using JLD2
+
+include("../../src/experiment.jl")
+
+# Launch worker processes.
+# num_cores = parse(Int, ENV["SLURM_CPUS_PER_TASK"])
+# addprocs(num_cores)
+# addprocs(3)
+
+adf, mdf, models = 
+    experiment(5; 
+        nbehaviors=[5, 20, 50], niter=20_000, 
+    whensteps = 1000);
+
+adf.pct_optimal = map(
+    r -> (haskey(r.countbehaviors_behavior, 1) ? 
+            r.countbehaviors_behavior[1] : 
+            0.0 )  / length(models[1].agents), 
+    eachrow(adf)
+)
+
+resdf = innerjoin(adf[!, Not([:countbehaviors_behavior])], 
+                  mdf, 
+                  on = [:ensemble, :step])
+
+resdf_trialsmean = combine(
+    # Groupby experimental variables...
+    groupby(resdf, [:step, :nbehaviors, :low_reliability, 
+            :high_reliability, :reliability_variance]),
+
+    # ...and aggregate by taking means over outcome variables, convert to table.
+    [:mean_soclearnfreq, :pct_optimal] 
+        =>
+            (
+                (soclearnfreq, pct_optimal) -> 
+                    (soclearnfreq = mean(soclearnfreq),
+                     pct_optimal = mean(pct_optimal))
+            ) 
+        =>
+            AsTable
+)
+
+resdf_trialsmean.baserels_nbehs = 
+    map(r -> string((r.high_reliability, r.low_reliability, r.nbehaviors)), 
+        eachrow(resdf_trialsmean))
+
+@save "trials_output.jld2" resdf_trialsmean
