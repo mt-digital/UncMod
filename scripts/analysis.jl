1243bfe1656f9fd35e2640a8303b785bdd984631
Matthew A. Turner <maturner01@gmail.com>
2021-11-28 21:26:24 -0800

    making analysis figures for interim report

--- scripts/analysis.jl
+++ scripts/analysis.jl
@@ -9,11 +9,10 @@ include("../src/experiment.jl")
 function plot_series(
     result; 
     start_step = 0, 
-    legendkeys = [:high_reliability, :low_reliability, :nbehaviors],
+    legendkeys = [:nbehaviors, :low_reliability, :high_reliability],
     outcomevar = :soclearnfreq,)
     
     if result isa String
-
         results_dict = load(result)
 
         if haskey(results_dict, "resdf_trialsmean")
@@ -27,21 +26,13 @@ function plot_series(
         end
     end
 
-    result.legendtuple = map(
-        r -> replace(
-            replace(string([r[el] for el in legendkeys]), "Real[" => ""), 
-            "]" => ""
-        ), 
-        eachrow(result)
-    )
+    # Create column that will be used for labeling each series.
+    result.legendtuple = make_legend_tuple(result, legendkeys)
 
     legendtitle = join([string(key) for key in legendkeys], ", ")
+
     if @isdefined models
-        if models[1].selection_strategy == Softmax
-            title = "τ = $(models[1].τ_init)\n$legendtitle"
-        else
-            title = "ϵ = $(models[1].ϵ_init)\n$legendtitle"
-        end
+        title = make_title(models)
     else
         title =  legendtitle
     end
@@ -66,7 +57,109 @@ function plot_series(
      )
 end
 
+function plot_final(result;
+    agg_start_step = 80_000,
+    xvar = :nbehaviors, yvar = :soclearnfreq, 
+    legendkeys = [:low_reliability, :high_reliability],
+    xticks = [5, 20, 100],
+    xlabel = nothing,
+    title = nothing,
+    legendtitle = nothing
+    )
 
+    if result isa String
+        results_dict = load(result)
 
+        if haskey(results_dict, "resdf_trialsmean")
+            results_dict["result"] = results_dict["resdf_trialsmean"]
+        end
 
+        result = results_dict["result"]
 
+        if haskey(results_dict, "models")
+            models = results_dict["models"]
+        end
+    end
+
+    # Make dataframe that takes mean yvar from agg_start_step to the end
+    # using split/apply/combine. See line 30 in scripts/slurm/run_trials.jl.
+    endtimesdf = filter(r -> r.step >= agg_start_step, result)
+
+    endtimesgroupby = combine(
+        groupby(endtimesdf, vcat(xvar, legendkeys)), 
+
+        [:soclearnfreq, :pct_optimal] =>
+        ((soclearnfreq, pct_optimal) ->
+            (soclearnfreq = mean(soclearnfreq),
+             pct_optimal = mean(pct_optimal))
+        ) =>
+        AsTable
+    )
+    
+    # Create column that will be used for labeling each series.
+    endtimesgroupby.legendtuple = make_legend_tuple(endtimesgroupby, legendkeys)
+
+    if isnothing(legendtitle)
+        legendtitle = join([string(key) for key in legendkeys], ", ")
+    end
+
+    if isnothing(xlabel)
+        xlabel = string(xvar)
+    end
+
+    if isnothing(title)
+        if @isdefined models
+            title = make_title(models)
+        else
+            title =  legendtitle
+        end
+    end
+
+    if yvar == :soclearnfreq
+        ylabel = "Social learning frequency"
+    else
+        ylabel = "% Optimal"
+    end
+
+    plot(
+        endtimesgroupby,
+        x = xvar, y = yvar, color = :legendtuple,
+        Geom.line, 
+        Geom.point, 
+        Theme(line_width=1.5pt),
+        Guide.xlabel(xlabel), Guide.ylabel(ylabel),
+        Guide.colorkey(title=legendtitle),
+        Guide.xticks(ticks=xticks), 
+        Guide.yticks(ticks=0.45:0.05:0.7), 
+        # Coord.cartesian(ymin=-0.05),
+        Guide.title(title),
+        shape=[Shape.diamond], Theme(point_size=4.5pt)
+     )
+        
+end
+
+function make_title(models)
+    if models[1].selection_strategy == Softmax
+        title = "τ = $(models[1].τ_init)\n$legendtitle"
+    else
+        title = "ϵ = $(models[1].ϵ_init)\n$legendtitle"
+    end
+end
+
+function make_legend_tuple(result, legendkeys)
+
+    map(
+        # Need to replace some cruft introduced when making string of list
+        # comprehension inside second replace.
+        r -> replace(
+            replace(
+                string([r[el] for el in legendkeys]), 
+                "[" => "("
+            ), 
+            "]" => ")"
+        ),
+
+        eachrow(result)
+    )
+
+end
