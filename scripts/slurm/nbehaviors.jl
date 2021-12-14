bf0db1975871c76106f8686e5588f0f57f1e12c5
Matthew Turner <maturner01@gmail.com>
2021-12-14 15:21:10 -0800

    running expected payoffs, nbehaviors, and roundsteps experiments on cluster

--- scripts/slurm/nbehaviors.jl
+++ scripts/slurm/nbehaviors.jl
@@ -1,7 +1,13 @@
 include("run_trials.jl")
 
-run_trials(2; 
-           steps_per_round = 50,
+tic = now()
+
+run_trials(10; 
+           steps_per_round = 100,
            nbehaviors = collect(5:5:100),
-           niter = 2_000, transledger = true, 
-           outputfilename = "data/firstdraft/nbehaviors.jld2")
+           niter = 100_000, transledger = false, 
+           outputfilename = "data/firstdraft/nbehaviors_10trials_notrans_100stepsper.jld2")
+
+trialstime = Dates.toms(now() - tic) / (60.0 * 1000.0)
+
+println("Ran expected payoffs trials in $trialstime minutes")
--- scripts/slurm/roundsteps.jl
+++ scripts/slurm/roundsteps.jl
@@ -1,7 +1,13 @@
 include("run_trials.jl")
 
-run_trials(2; 
-           steps_per_round = [5, 20],
+tic = now()
+
+run_trials(100; 
+           steps_per_round = [5, 10, 20, 50, 100],
            nbehaviors = [5, 20, 100],
-           niter = 10_000, transledger = true, 
+           niter = 100_000, transledger = true, 
            outputfilename = "data/firstdraft/roundsteps.jld2")
+
+trialstime = Dates.toms(now() - tic) / (60.0 * 1000.0)
+
+println("Ran expected payoffs trials in $trialstime minutes")
