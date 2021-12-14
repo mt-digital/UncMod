b7ed7aad4be49cc99375c1c1e69fe13ab57f28b0
Matthew Turner <maturner01@gmail.com>
2021-12-14 04:19:07 -0800

    100 trials with 4h wall time

--- scripts/slurm/expectedpayoffs.jl
+++ scripts/slurm/expectedpayoffs.jl
@@ -1,9 +1,16 @@
 include("run_trials.jl")
 
-run_trials(2; 
+tic = now()
+
+run_trials(100; 
            steps_per_round = 50,
-           nbehaviors = [5, 20, 100],
+           # nbehaviors = [5, 20, 100],
+           nbehaviors = 20,
            high_reliability = collect(0.1:0.1:0.9),
            low_reliability =  collect(0.1:0.1:0.9),
-           niter = 2_000, transledger = true, 
+           niter = 100_000, transledger = true, 
            outputfilename = "data/firstdraft/expectedpayoffs.jld2")
+
+trialstime = Dates.toms(now() - tic) / (60.0 * 1000.0)
+
+println("Ran expected payoffs trials in $trialstime minutes")
