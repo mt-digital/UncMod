5fe68634b1c5d760700bbfde21ac2534acc2f9ff
Matthew Turner <maturner01@gmail.com>
2022-08-18 17:11:41 -0700

    updates from remote

--- nteachers.sh
+++ nteachers.sh
@@ -1,10 +1,10 @@
 # Run 1000 trials for N_T=2,10,20.
-sbatch --array=1-10 scripts/slurm/nteachers_sensitivity/B\=2_nteachers\=2.slurm
-sbatch --array=1-10 scripts/slurm/nteachers_sensitivity/B\=2_nteachers\=10.slurm
-sbatch --array=1-10 scripts/slurm/nteachers_sensitivity/B\=2_nteachers\=20.slurm
-sbatch --array=1-10 scripts/slurm/nteachers_sensitivity/B\=4_nteachers\=2.slurm
-sbatch --array=1-10 scripts/slurm/nteachers_sensitivity/B\=4_nteachers\=10.slurm
-sbatch --array=1-10 scripts/slurm/nteachers_sensitivity/B\=4_nteachers\=20.slurm
-sbatch --array=1-10 scripts/slurm/nteachers_sensitivity/B\=10_nteachers\=2.slurm
-sbatch --array=1-10 scripts/slurm/nteachers_sensitivity/B\=10_nteachers\=10.slurm
-sbatch --array=1-10 scripts/slurm/nteachers_sensitivity/B\=10_nteachers\=20.slurm
+sbatch --array=1-100 scripts/slurm/nteachers_sensitivity/B\=2_nteachers\=2.slurm
+sbatch --array=1-100 scripts/slurm/nteachers_sensitivity/B\=2_nteachers\=10.slurm
+sbatch --array=1-100 scripts/slurm/nteachers_sensitivity/B\=2_nteachers\=20.slurm
+sbatch --array=1-100 scripts/slurm/nteachers_sensitivity/B\=4_nteachers\=2.slurm
+sbatch --array=1-100 scripts/slurm/nteachers_sensitivity/B\=4_nteachers\=10.slurm
+sbatch --array=1-100 scripts/slurm/nteachers_sensitivity/B\=4_nteachers\=20.slurm
+sbatch --array=1-100 scripts/slurm/nteachers_sensitivity/B\=10_nteachers\=2.slurm
+sbatch --array=1-100 scripts/slurm/nteachers_sensitivity/B\=10_nteachers\=10.slurm
+sbatch --array=1-100 scripts/slurm/nteachers_sensitivity/B\=10_nteachers\=20.slurm
--- tau.sh
+++ tau.sh
@@ -1,7 +1,7 @@
 # Run 1000 trials for both softmax sensitivity analyses.
-sbatch --array=1-10 scripts/slurm/tau_sensitivity/B\=2_tau\=0.01.slurm
-sbatch --array=1-10 scripts/slurm/tau_sensitivity/B\=4_tau\=0.01.slurm
-sbatch --array=1-10 scripts/slurm/tau_sensitivity/B\=10_tau\=0.01.slurm
-sbatch --array=1-10 scripts/slurm/tau_sensitivity/B\=2_tau\=1.0.slurm
-sbatch --array=1-10 scripts/slurm/tau_sensitivity/B\=4_tau\=1.0.slurm
-sbatch --array=1-10 scripts/slurm/tau_sensitivity/B\=10_tau\=1.0.slurm
+sbatch --array=1-90 scripts/slurm/tau_sensitivity/B\=2_tau\=0.01.slurm
+sbatch --array=1-90 scripts/slurm/tau_sensitivity/B\=4_tau\=0.01.slurm
+sbatch --array=1-90 scripts/slurm/tau_sensitivity/B\=10_tau\=0.01.slurm
+sbatch --array=1-90 scripts/slurm/tau_sensitivity/B\=2_tau\=1.0.slurm
+sbatch --array=1-90 scripts/slurm/tau_sensitivity/B\=4_tau\=1.0.slurm
+sbatch --array=1-90 scripts/slurm/tau_sensitivity/B\=10_tau\=1.0.slurm
